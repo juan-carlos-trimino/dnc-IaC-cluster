2f510f9b590427d1c1a288ecae7fd1ceab518b2b
Juan Carlos Trimino <juancarlos@trimino.com>
2023-05-22 22:21:42 -0500

    Add modules.

--- tf/bootstrap.tf
+++ tf/bootstrap.tf
@@ -10,6 +10,7 @@ locals {
   # Name of Services #
   ####################
   svc_redis = "dnc-redis"
+  svc_redis_app = "dnc-redis-app"
   svc_storage = "dnc-storage"
   svc_storage_cs = "dnc-storage-cs"
   svc_rmq_subscriber = "dnc-rmq-subscriber"
@@ -18,7 +19,7 @@ locals {
   ############
   # Services #
   ############
-  svc_dns_redis = "${local.svc_redis}.${local.namespace}.svc.cluster.local"
+  # svc_dns_redis = "${local.svc_redis}.${local.namespace}.svc.cluster.local"
   svc_dns_storage = "${local.svc_storage}.${local.namespace}.svc.cluster.local"
   svc_dns_storage_cs = "${local.svc_storage_cs}.${local.namespace}.svc.cluster.local"
   # By default, the guest user is prohibited from connecting from remote hosts; it can only connect
@@ -38,26 +39,24 @@ module "dnc-redis" {
   source = "./modules/redis-statefulset"
   app_name = var.app_name
   app_version = var.app_version
-  image_tag = "redis:7.0.11-alpine"
+  # image_tag = "redis:7.0.11-alpine"
+  # image_tag = "redis:7.0.11"
+  image_tag = "redis/redis-stack:edge"
   image_pull_policy = "IfNotPresent"
   namespace = local.namespace
-  # path_rabbitmq_files = "./modules/rabbitmq-statefulset/utilxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-  # rabbitmq_erlang_cookie = var.rabbitmq_erlang_cookie
-  # rabbitmq_default_pass = var.rabbitmq_default_pass
-  # rabbitmq_default_user = var.rabbitmq_default_user
+  path_redis_files = "./modules/redis-statefulset/util"
   publish_not_ready_addresses = true
   # Because several features (e.g. quorum queues, client tracking in MQTT) require a consensus
   # between cluster members, odd numbers of cluster nodes are highly recommended: 1, 3, 5, 7
   # and so on.
-  replicas = 1
+  replicas = 3
   # Limits and requests for CPU resources are measured in millicores. If the container needs one
   # full core to run, use the value '1000m.' If the container only needs 1/4 of a core, use the
   # value of '250m.'
   qos_limits_cpu = "500m"
   qos_limits_memory = "800Mi"
   pvc_access_modes = ["ReadWriteOnce"]
-  pvc_storage_size = "10Gi"
-  # pvc_storage_class_name = "ibmc-vpc-block-general-purpose"
+  pvc_storage_size = "5Gi"
   pvc_storage_class_name = "ibmc-block-silver"
   env = {
     # If a system uses fully qualified domain names (FQDNs) for hostnames, RabbitMQ nodes and CLI
@@ -69,8 +68,57 @@ module "dnc-redis" {
   redis_service_port = 6379
   redis_service_target_port = 6379
   service_name = local.svc_redis
+  # service_type = "LoadBalancer"
 }
 # ***/  # redis - stateful
+
+
+
+
+module "dnc-redis-app" {
+  depends_on = [
+    module.dnc-redis
+  ]
+  source = "./modules/deployment"
+  dir_name = "../../dnc-redis"
+  app_name = var.app_name
+  app_version = var.app_version
+  namespace = local.namespace
+  # image_tag = "redis:7.0.11-alpine"
+  # image_tag = "redis:7.0.11"
+  # image_pull_policy = "IfNotPresent"
+  # path_redis_files = "./modules/redis-statefulset/util"
+  # publish_not_ready_addresses = true
+  # Because several features (e.g. quorum queues, client tracking in MQTT) require a consensus
+  # between cluster members, odd numbers of cluster nodes are highly recommended: 1, 3, 5, 7
+  # and so on.
+  replicas = 1
+  # Limits and requests for CPU resources are measured in millicores. If the container needs one
+  # full core to run, use the value '1000m.' If the container only needs 1/4 of a core, use the
+  # value of '250m.'
+  qos_limits_cpu = "500m"
+  qos_limits_memory = "800Mi"
+  cr_login_server = local.cr_login_server
+  cr_username = var.cr_username
+  cr_password = var.cr_password
+  # pvc_access_modes = ["ReadWriteOnce"]
+  # pvc_storage_size = "5Gi"
+  # pvc_storage_class_name = "ibmc-block-silver"
+  env = {
+    # If a system uses fully qualified domain names (FQDNs) for hostnames, RabbitMQ nodes and CLI
+    # tools must be configured to use so called long node names.
+    # RABBITMQ_USE_LONGNAME = true
+    # Override the main RabbitMQ config file location.
+    # RABBITMQ_CONFIG_FILE = "/config/rabbitmq"
+  }
+  # redis_service_port = 637
+  # redis_service_target_port = 6379
+  service_name = local.svc_redis_app
+  # service_type = "LoadBalancer"
+}
+
+
+
 ###################################################################################################
 # rabbitmq                                                                                        #
 ###################################################################################################
@@ -253,7 +301,7 @@ module "dnc-storage-cs" {
   cr_password = var.cr_password
   env = {
     # SVC_DNS: local.svc_dns_storage_cs
-    BUCKET_NAME: var.bucket_name
+    # BUCKET_NAME: var.bucket_name
     # Without HMAC.
     # AUTHENTICATION_TYPE: "iam"
     # API_KEY: var.storage_api_key
